1f02a61e5caf01f1cfe00ebc0771c18e3980f6e7
Henrico Pops <henricopops@gmail.com>
2022-05-11 19:25:07 +0200

    Changes

--- TouchController/MainWindow.xaml.cs
+++ TouchController/MainWindow.xaml.cs
@@ -1,10 +1,15 @@
-﻿using SocketIOClient;
+﻿using RtMidi.Core;
+using RtMidi.Core.Devices;
+using RtMidi.Core.Messages;
+using SocketIOClient;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -36,6 +41,26 @@ namespace TouchController
         public float Strength;
     }
 
+    public class TouchJson
+    {
+        [JsonPropertyName("width")]
+        public float Width { get; set; }
+        [JsonPropertyName("height")]
+        public float Height { get; set; }
+        [JsonPropertyName("touchpoints")]
+        public TouchPointJson Points { get; set; }
+    }
+
+    public class TouchPointJson
+    {
+        [JsonPropertyName("id")]
+        public object Id { get; set; }
+        [JsonPropertyName("x")]
+        public float X { get; set; }
+        [JsonPropertyName("y")]
+        public float Y { get; set; }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -44,6 +69,10 @@ namespace TouchController
         const int WS_EX_TRANSPARENT = 0x00000020;
         const int GWL_EXSTYLE = (-20);
         public const uint WS_EX_LAYERED = 0x00080000;
+
+        public bool[] Midi;
+        public int MidiSize = 7;
+        public int[] Mapping = new int[] { 48, 50, 49, 51, 52,  54, 53, };
 
         [DllImport("user32.dll")]
         static extern int GetWindowLong(IntPtr hwnd, int index);
@@ -54,6 +83,8 @@ namespace TouchController
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        public Dictionary<string, Ellipse> VisibleTouch = new Dictionary<string, Ellipse>();
 
         public static Point GetMousePosition()
         {
@@ -71,6 +102,7 @@ namespace TouchController
 
         private bool _isClickThrough = true;
         private SocketIO? client;
+        private IMidiOutputDevice outputDevice;
 
         public MainWindow()
         {
@@ -79,8 +111,20 @@ namespace TouchController
             //  DispatcherTimer setup
             var dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 200);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 5);
             dispatcherTimer.Start();
+
+            Midi = new bool[MidiSize];
+
+
+            foreach (var outputDeviceInfo in MidiDeviceManager.Default.OutputDevices)
+            {
+                Console.WriteLine($"Opening {outputDeviceInfo.Name}");
+
+                outputDevice = outputDeviceInfo.CreateDevice();
+
+                outputDevice.Open();
+            }
         }
         public Size GetElementPixelSize(UIElement element)
         {
@@ -116,70 +160,112 @@ namespace TouchController
             r.Width = Math.Round(size.Width);
             r.Height = Math.Round(size.Height);
 
+
+
             if (r.Contains(p1) && client != null)
             {
                 Trace.WriteLine("x" + p1);
                 var dto = new Touch { AreaSize = new Vector((int)Math.Round(size.Width), (int)Math.Round(size.Height)) };
                 await client.EmitAsync("touch", "source", dto);
+                 
+                var newMidi = new bool[MidiSize];
+                var activeFloat = (p1.X - r.Left) / r.Width * MidiSize;
+                var activeIndex = (int)Math.Round(activeFloat) - 1;
+                Trace.WriteLine(activeIndex);
+                if (activeIndex >= 0)
+                    newMidi[activeIndex] = true;
+                UpdateMidi(newMidi);
             }
             else
             {
+                UpdateMidi(new bool[MidiSize]);
             }
+
+        }
+
+        private void UpdateMidi(bool[] vs)
+        {
+            var line = "";
+            for (int i = 0; i < vs.Length; i++)
+            {
+                line += vs[i] + ",";
+            }
+
+ // Trace.WriteLine(line);
+
+            for (int i = 0; i < vs.Length; i++)
+            {
+                if (!vs[i] && Midi[i])
+                {
+                    var message = new NoteOffMessage(RtMidi.Core.Enums.Channel.Channel1, (RtMidi.Core.Enums.Key)(Mapping[i]), 0);
+                    outputDevice.Send(message);
+                }
+            }
+
+            for (int i = 0; i < vs.Length; i++)
+            {
+                if (vs[i] && !Midi[i])
+                {
+                    var message = new NoteOnMessage(RtMidi.Core.Enums.Channel.Channel1, (RtMidi.Core.Enums.Key)(Mapping[i]), 10);
+                    outputDevice.Send(message);
+                }
+            }
+
+            Midi = vs;
         }
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            client = new SocketIO("http://localhost:11000/");
+            client = new SocketIO("http://168.119.29.140:3000/");
             Trace.WriteLine("WindowLoaded");
-            client.On("hi", response =>
-            {
-                // You can print the returned data first to decide what to do next.
-                // output: ["hi client"]
-                Console.WriteLine(response);
 
-                string text = response.GetValue<string>();
 
-                // The socket.io server code looks like this:
-                // socket.emit('hi', 'hi client');
-            });
-
-            client.On("touch", response =>
+            client.On("touchpoints", response =>
             {
                 // You can print the returned data first to decide what to do next.
                 // output: ["hi client"]
                 Trace.WriteLine(response);
-
-                string text = response.GetValue<string>();
-
                 // The socket.io server code looks like this:
                 // socket.emit('hi', 'hi client');
-            });
+                var jsonString = response.ToString();
+                TouchJson? jsonObject = JsonSerializer.Deserialize<TouchJson[]>(jsonString)[0];
 
-            client.On("test", response =>
-            {
-                // You can print the returned data first to decide what to do next.
-                // output: ["ok",{"id":1,"name":"tom"}]
-                Console.WriteLine(response);
+                Application.Current.Dispatcher.Invoke((Action)delegate
+                {
 
-                // Get the first data in the response
-                string text = response.GetValue<string>();
-                // Get the second data in the response
-                var dto = response.GetValue<Touch>(1);
+                    foreach (var circle in VisibleTouch)
+                    {
+                        VisualGrid.Children.Remove(circle.Value);
+                    }
+                    VisibleTouch.Clear();
 
-                // The socket.io server code looks like this:
-                // socket.emit('hi', 'ok', { id: 1, name: 'tom'});
+                    if (jsonObject != null)
+                        foreach (var p in new List<TouchPointJson>() { jsonObject.Points })
+                        {
+
+                            var m = new Thickness(VisualBox.Margin.Left + (p.X / jsonObject.Width) * VisualBox.Width ,
+                                VisualBox.Margin.Top + (p.Y / jsonObject.Height) * VisualBox.Height, 0,0);
+                            var circle = new Ellipse()
+                            {
+                                Width = 15,
+                                Height = 15,
+                                Margin = m,
+                                Fill = new SolidColorBrush(Colors.White),
+                                HorizontalAlignment = HorizontalAlignment.Left,
+                                VerticalAlignment = VerticalAlignment.Top
+                            };
+                            VisualGrid.Children.Add(circle);
+                            VisibleTouch.Add(p.Id.ToString(), circle);
+                        }
+                });
+
             });
 
             client.OnConnected += async (sender, e) =>
             {
                 Trace.WriteLine("Connected");
-                // Emit a string
-                await client.EmitAsync("hi", "socket.io");
-
-                // Emit a string and an object
-
-                //var dto = new Touch { AreaSize = new Vector((int)Math.Round(this.VisualBox.ActualWidth), (int)Math.Round(this.VisualBox.Height)) };
-               // await client.EmitAsync("register", "source", dto);
+                // Set this client as output device
+                await client.EmitAsync("device", "output");
             };
             try
             {
